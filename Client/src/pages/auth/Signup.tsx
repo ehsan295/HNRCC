67e6e698b4f9a4f428904aa817cb53586b5a0287
Nooruddin <75014277+nooruddin-rahmani@users.noreply.github.com>
2023-10-22 12:34:54 +0430

    signup changed

--- Client/src/pages/auth/Signup.tsx
+++ Client/src/pages/auth/Signup.tsx
@@ -9,28 +9,18 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormDescription,
+  FormMessage
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
-
-const onSubmit:any = async (values:z.infer<typeof formSchema>) => {
-  
-  try {
-    axios.post('http://localhost:3000/signup', values)
-  console.log('Posted sucesfully')
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-
 const formSchema = z.object({
-  name: z.string().min(2).max(20),
-  email:z.string().email(),
-  password:z.string().min(2).max(20),
-  confirmPassword:z.string().min(2).max(20)
+  name: z.string().min(1,'لطفا نام را وارد کنید.').min(2,' نام باید حداقل ۲ نویسه باشد.').max(20, 'نام باید حداکثر ۲۰ نویسه باشد.'),
+  email:z.string().min(3, 'لطفا ایمیل را وارد کنید.').email('ابمبل نامعتبر.'),
+  password:z.string().min(1,'لطفا رمز عبور را وارد کنید.').min(6,'رمز عبور باید حداقل ۶ نویسه باشد.').max(20, 'رمز عبور باید حداکثر ۲۰ نویسه باشد.'),
+  confirmPassword:z.string().min(1,'لطفا رمز عبور را وارد کنید.').min(6,'رمز عبور باید حداقل ۶ نویسه باشد.').max(20, 'رمز عبور باید حداکثر ۲۰ نویسه باشد.'),
 })
 .refine((data)=> data.password === data.confirmPassword, {
     message:"رمز عبور یکسان نیست",
@@ -41,77 +31,85 @@ const formSchema = z.object({
   
 
 function Signup() {
-    const form = useForm()
+      const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email:'',
+      password:'',
+      confirmPassword:''
+
+    },
+  })
+
+    // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+
+    console.log(values)
+  }
   return (
     <div className="w-screen h-screen flex justify-center mx-auto items-center">
        <div className='w-96 p-10 bg-slate-200 rounded-md space-y-8'>
         <div className=''>
           <img className='mx-auto w-16' src='./vite.svg'/>
         </div>
-         <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 ">
-      <div className=''>
-            
-      <FormField
-        control={form.control}
-        name="name"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>نام</FormLabel>
-            <FormControl>
-              <Input placeholder="نام" {...field} />
-            </FormControl>
-            
-          </FormItem>
-          
-        )}
-      />
-            <FormField
-        control={form.control}
-        name="email"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>ایمیل</FormLabel>
-            <FormControl>
-              <Input placeholder="ایمیل" type='email' {...field} />
-            </FormControl>
-            
-          </FormItem>
-          
-        )}
-      />
-            <FormField
-        control={form.control}
-        name="password"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>رمز عبور</FormLabel>
-            <FormControl>
-              <Input placeholder="رمز عبور" type='password' {...field} />
-            </FormControl>
-            
-          </FormItem>
-          
-        )}
-      />
-            <FormField
-        control={form.control}
-        name="confirm-password"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>تکرار رمز عبور</FormLabel>
-            <FormControl>
-              <Input placeholder="تکرار رمز عبور" type='password' {...field} />
-            </FormControl>
-            
-          </FormItem>
-          
-        )}
-      />
-      </div>
-      <Button className='w-full' type="submit">ثبت نام</Button>
-    </form>
-  </Form>
+          <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>نام</FormLabel>
+              <FormControl>
+                <Input placeholder="نام" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+                <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>ایمیل</FormLabel>
+              <FormControl>
+                <Input placeholder="ایمیل" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+                        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>رمز عبور</FormLabel>
+              <FormControl>
+                <Input type='password' placeholder="رمز عبور" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+                        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>تایید رمز عبور</FormLabel>
+              <FormControl>
+                <Input type='password' placeholder="تایید رمز عبور" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className='w-full' type="submit">ثبت نام</Button>
+      </form>
+    </Form>
   <div className="my-1">
   <Link className='hover:text-blue-800 active:text-indigo-800 font-medium' to='/signin'>حساب کاربری دارید؟</Link>
   </div>
