52fdf3f3f3eab4b2fd02ab9c785749d834ae4a85
Nooruddin <75014277+nooruddin-rahmani@users.noreply.github.com>
2023-10-26 18:03:05 +0430

    added the changes route to app

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,26 +1,25 @@
-
 import Signup from "./pages/auth/Signup";
 import Signin from "./pages/auth/Signin";
 import CRM from "./pages/CRM";
-import NotFound from './pages/NotFound'
+import NotFound from "./pages/NotFound";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SucessSignup from "./pages/auth/SucessSignup";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='*' element={<NotFound/>} />
-      <Route path='/' element={<Signup/>} />  
-      <Route path='/crm' element={<CRM/>} />  
-      <Route path='/sucess' element={<SucessSignup/>} />  
-      <Route path='/signup' element={<Signup/>} />
-      <Route path='/signin' element={<Signin/>} />
-    </Routes>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Signup />} />
+        <Route path="/crm" element={<CRM />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sucess" element={<SucessSignup />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
-
