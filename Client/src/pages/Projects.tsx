9e7abc764b33c93c35c3068a09e99edcfd5e3753
Nooruddin <75014277+nooruddin-rahmani@users.noreply.github.com>
2023-10-26 18:02:41 +0430

    added a rotected route called projects

--- Client/src/pages/Projects.tsx
+++ Client/src/pages/Projects.tsx
@@ -1,7 +1,28 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 function Projects() {
-  return (
-    <div>Projects</div>
-  )
+  const navigate = useNavigate();
+  const accessToken = localStorage.getItem("token");
+  useEffect(() => {
+    if (accessToken == null) {
+      navigate("/signin");
+    } else {
+      const apiURL = "http://localhost:5000/api/v1";
+      const authAxios = axios.create({
+        baseURL: apiURL,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const getData = async () => {
+        const responce = await authAxios.get("/projects");
+      };
+    }
+  }, [accessToken]);
+
+  return <div>Projects</div>;
 }
 
-export default Projects
+export default Projects;
