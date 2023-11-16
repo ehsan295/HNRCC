53edea1a189c3cd1c81f4f179f7fdaedc41fb704
ehsan295 <81233735+ehsan295@users.noreply.github.com>
2023-11-16 20:13:06 +0430

    correct product model

--- Server/src/models/product.js
+++ Server/src/models/product.js
@@ -2,30 +2,53 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../database/index");
 
 const Product = sequelize.define("product", {
+  productId: {
+   type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement:true,
+    primaryKey:true,
+  },
+  date: {
+   type: DataTypes.DATE,
+    allowNull: false,
+  },
+  detail: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
   name: {
     type: DataTypes.STRING,
-    allowNull: false,
-  },
-  details: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  volume: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  productCode: {
-    type: DataTypes.NUMBER,
-    allowNull: false,
-  },
-  price: {
-    type: DataTypes.NUMBER,
     allowNull: false,
   },
   unit: {
     type: DataTypes.STRING,
     allowNull: false,
   },
+
+volume: {
+  type: DataTypes.INTEGER,
+  allowNull: false,
+},
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  picture: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+    projectId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references:{
+        model:project,
+        key:'projectId'
+      }
+    },
 });
+product.belongsTo(project,
+  {
+    foreignkey:'projectId'
+  });
 
 module.exports = Product;
