1c00993161901ebba29e20845c9c2339a91d5eb3
Nooruddin <75014277+nooruddin-rahmani@users.noreply.github.com>
2023-11-17 23:05:02 +0430

    updated the endpoints

--- Server/src/api/index.js
+++ Server/src/api/index.js
@@ -1,12 +1,21 @@
-const express = require('express');
-const signupAPI  = require('./signup')
-const signinAPI = require('./signin')
-const projectsAPI = require('./projects')
+const express = require("express");
+const signupAPI = require("./signup");
+const signinAPI = require("./signin");
+const projectsAPI = require("./projects");
+const createProductAPI = require("./product/createProduct");
+const readProductAPI = require("./product/readProduct");
+const updateProductAPI = require("./product/updateProduct");
+const deleteProductAPI = require("./product/daleteProduct");
 const router = express.Router();
 
-router.use(signupAPI)
-router.use(signinAPI)
-router.use(projectsAPI)
+router.use(signupAPI);
+router.use(signinAPI);
+router.use(projectsAPI);
 
+//product
+router.use(createProductAPI);
+router.use(updateProductAPI);
+router.use(readProductAPI);
+router.use(deleteProductAPI);
 
 module.exports = router;
--- Server/src/database/index.js
+++ Server/src/database/index.js
@@ -1,22 +1,13 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-    }
-  );
-  sequelize.sync()
-//26:14
-// (async ()=>{
-//     try{
-//         await sequelize.authenticate()
-//         console.log('Conection established')
-//     }catch(error){
-//         console.log('Could not Connect', error)
-//     }
-// })();
-module.exports = sequelize
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
+sequelize.sync();
+module.exports = sequelize;
