d73f57a1fcb955dbc783fdde57820f0266446e0a
ehsan295 <81233735+ehsan295@users.noreply.github.com>
2023-11-18 21:13:45 +0430

    add insertation deletion and update and read of da

---
+++ Server/src/api/employee/updateEmpolyee.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const Employee = require("../../models/empolyee");
+const router = express.Router();
+
+router.put("/employee/:id", async (req, res) => {
+  const empolyeId = req.params.id;
+
+  try {
+    const updatedEmployee = await Employee.findOneAndUpdate(
+      { id: empolyeId },
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    if (updatedEmployee) {
+      res.json({
+        message: "داده های کارمند موفقانه ویرایش شد",
+        updatedProduct,
+      });
+    } else {
+      res.json({
+        message: "کارمند موجود نیست",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+});
+
+module.exports = router;
