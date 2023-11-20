a2cd70a513e983953cc8eda06f87bae5d27df4b0
ehsan295 <81233735+ehsan295@users.noreply.github.com>
2023-11-20 11:38:57 +0430

    without error

--- Server/src/api/index.js
+++ Server/src/api/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const signupAPI = require("./signup");
 const signinAPI = require("./signin");
 //product
-const createProductAPI = require("./product/createProduct");
+// const createProductAPI = require("./product/createProduct");
 // const readProductAPI = require("./product/readProduct");
 // const updateProductAPI = require("./product/updateProduct");
 // const deleteProductAPI = require("./product/deleteProduct");
 // //project
-const createProjectAPI = require("./project/createProject");
+// const createProjectAPI = require("./project/createProject");
 // const deleteProjectAPI = require("./project/deleteProject");
 // const readProjectAPI = require("./project/readProject");
 // const updateProjectAPI = require("./project/updateProject");
@@ -78,13 +78,13 @@ router.use(signupAPI);
 router.use(signinAPI);
 
 // //product
-router.use(createProductAPI);
+// router.use(createProductAPI);
 // router.use(updateProductAPI);
 // router.use(readProductAPI);
 // router.use(deleteProductAPI);
 
 // //product
-router.use(createProjectAPI);
+// router.use(createProjectAPI);
 // router.use(updateProjectAPI);
 // router.use(readProjectAPI);
 // router.use(deleteProjectAPI);
--- Server/src/database/createTable.js
+++ Server/src/database/createTable.js
@@ -1,18 +1,18 @@
 const sequelize = require("../database/index");
 const ProductModel = require("../models/product");
 const ProjectModel = require("../models/project");
-const VehicleModel = require ("../models/vehicle");
+// const VehicleModel = require ("../models/vehicle");
 const EmployeeModel = require ("../models/empolyee");
-const TransferModel = require("../models/transfer");
-const OrderModel = require ("../models/order");
-const StockModel = require("../models/stock")
-const PurchaseModel = require("../models/purches")
-const OrderdetailModel = require ("../models/orderdetail")
-const PaymentModel =require("../models/payment")
-const CustomerModel = require("../models/customer")
-const OrderdetailProductModel = require("../models/orderdetailProduct")
-const ExpenseCategoryModel = require("../models/expenseCatagore")
-const ExpenseModel= require("../models/expenseCatagore")
+// const TransferModel = require("../models/transfer");
+// const OrderModel = require ("../models/order");
+// const StockModel = require("../models/stock")
+// const PurchaseModel = require("../models/purches")
+// const OrderdetailModel = require ("../models/orderdetail")
+// const PaymentModel =require("../models/payment")
+// const CustomerModel = require("../models/customer")
+// const OrderdetailProductModel = require("../models/orderdetailProduct")
+// const ExpenseCategoryModel = require("../models/expenseCatagore")
+// const ExpenseModel= require("../models/expenseCatagore")
 
 
 
@@ -25,62 +25,62 @@ ProductModel.belongsTo(ProjectModel, {
 });
 
 // vehicle and project
-ProjectModel.hasMany(VehicleModel, {
-  foreignKey: "projectId",
-});
-VehicleModel.belongsTo(ProjectModel,{
-  foreignKey: "projectId",
-})
-// vehicle and Employee
-EmployeeModel.hasOne(VehicleModel,{
-  foreignKey: "employeId",
-})
-VehicleModel.belongsTo(EmployeeModel,{
-  foreignKey: "driverId",
-})
+// ProjectModel.hasMany(VehicleModel, {
+//   foreignKey: "projectId",
+// });
+// VehicleModel.belongsTo(ProjectModel,{
+//   foreignKey: "projectId",
+// })
+// // vehicle and Employee
+// EmployeeModel.hasOne(VehicleModel,{
+//   foreignKey: "employeId",
+// })
+// VehicleModel.belongsTo(EmployeeModel,{
+//   foreignKey: "driverId",
+// })
 
 
 //transfer and vehicle
-VehicleModel.hasMany(TransferModel,{
-  foreignKey: "vehicleId",
-})
-TransferModel.belongsTo(VehicleModel,{
-  foreignKey: "vehicleId",
-})
-// order and transfer
-OrderModel.hasMany(TransferModel, {
-  foreignKey: 'orderId',
-});
+// VehicleModel.hasMany(TransferModel,{
+//   foreignKey: "vehicleId",
+// })
+// TransferModel.belongsTo(VehicleModel,{
+//   foreignKey: "vehicleId",
+// })
+// // order and transfer
+// OrderModel.hasMany(TransferModel, {
+//   foreignKey: 'orderId',
+// });
 
-TransferModel.belongsTo(OrderModel, {
-  foreignKey: 'orderId',
-});
-// stock and project
-ProjectModel.hasMany(StockModel, {
-  foreignKey: 'projectId',
-});
+// TransferModel.belongsTo(OrderModel, {
+//   foreignKey: 'orderId',
+// });
+// // stock and project
+// ProjectModel.hasMany(StockModel, {
+//   foreignKey: 'projectId',
+// });
 
-StockModel.belongsTo(ProjectModel, {
-  foreignKey: 'projectId',
-});
+// StockModel.belongsTo(ProjectModel, {
+//   foreignKey: 'projectId',
+// });
 
-// Set up the association between PurchaseModel and ProjectModel
-PurchaseModel.belongsTo(ProjectModel, {
-  foreignKey: 'projectId',
-});
+// // Set up the association between PurchaseModel and ProjectModel
+// PurchaseModel.belongsTo(ProjectModel, {
+//   foreignKey: 'projectId',
+// });
 
-ProjectModel.hasMany(PurchaseModel, {
-  foreignKey: 'projectId',
-});
+// ProjectModel.hasMany(PurchaseModel, {
+//   foreignKey: 'projectId',
+// });
 
 //purches and employee
-PurchaseModel.belongsTo(EmployeeModel, {
-  foreignKey: 'employeId',
-});
+// PurchaseModel.belongsTo(EmployeeModel, {
+//   foreignKey: 'employeId',
+// });
 
-EmployeeModel.hasMany(PurchaseModel, {
-  foreignKey: 'employeId',
-});
+// EmployeeModel.hasMany(PurchaseModel, {
+//   foreignKey: 'employeId',
+// });
 //product and project
 ProductModel.belongsTo(ProjectModel, {
   foreignKey: 'projectId',
@@ -89,74 +89,74 @@ ProductModel.belongsTo(ProjectModel, {
 ProjectModel.hasMany(ProductModel, {
   foreignKey: 'projectId',
 });
-// payment and order
-OrderModel.belongsToMany(PaymentModel, {
-  through: 'PaymentOrder',
-  foreignKey: 'orderId',
-});
+// // payment and order
+// OrderModel.belongsToMany(PaymentModel, {
+//   through: 'PaymentOrder',
+//   foreignKey: 'orderId',
+// });
 
-//  PaymentModel and CustomerModel
+// //  PaymentModel and CustomerModel
 
-CustomerModel.hasMany(PaymentModel, {
-  // foreignKey: 'customerId', 
-
-
+// CustomerModel.hasMany(PaymentModel, {
+//   // foreignKey: 'customerId', 
 
 
-});
-PaymentModel.belongsTo(CustomerModel, {
-  foreignKey: 'customerId',
+
+
+// });
+// PaymentModel.belongsTo(CustomerModel, {
+//   foreignKey: 'customerId',
   
 
-});
+// });
 
 //orderdetail and order
-OrderdetailModel.belongsTo(OrderModel, {
-  foreignKey: 'orderId',
-});
+// OrderdetailModel.belongsTo(OrderModel, {
+//   foreignKey: 'orderId',
+// });
 
-OrderdetailModel.belongsTo(ProductModel, {
-  foreignKey: 'productId',
-});
-//orderdetail and customer
-OrderdetailModel.belongsTo(CustomerModel, {
-  foreignKey: 'customerId',
-});
-CustomerModel.hasMany(OrderdetailModel, {
-  foreignKey: 'customerId',
-});
-// orderdetail and product
-OrderdetailModel.belongsToMany(ProductModel, {
-  through: OrderdetailProductModel,
-  foreignKey: 'orderdetailId',
-});
+// OrderdetailModel.belongsTo(ProductModel, {
+//   foreignKey: 'productId',
+// });
+// //orderdetail and customer
+// OrderdetailModel.belongsTo(CustomerModel, {
+//   foreignKey: 'customerId',
+// });
+// CustomerModel.hasMany(OrderdetailModel, {
+//   foreignKey: 'customerId',
+// });
+// // orderdetail and product
+// OrderdetailModel.belongsToMany(ProductModel, {
+//   through: OrderdetailProductModel,
+//   foreignKey: 'orderdetailId',
+// });
 
-ProductModel.belongsToMany(OrderdetailModel, {
-  through: OrderdetailProductModel,
-  foreignKey: 'productId',
-});
+// ProductModel.belongsToMany(OrderdetailModel, {
+//   through: OrderdetailProductModel,
+//   foreignKey: 'productId',
+// });
 
-// expense and project and empolyee
-ProjectModel.hasMany(ExpenseModel, {
-  foreignKey: 'projectId',
-})
-ExpenseModel.belongsTo(ProjectModel, {
-  foreignKey: 'projectId',
-});
+// // expense and project and empolyee
+// ProjectModel.hasMany(ExpenseModel, {
+//   foreignKey: 'projectId',
+// })
+// ExpenseModel.belongsTo(ProjectModel, {
+//   foreignKey: 'projectId',
+// });
 
-ExpenseCategoryModel.hasMany(ExpenseModel, {
-  foreignKey: 'expenseCategoryid',
-})
-ExpenseModel.belongsTo(ExpenseCategoryModel, {
-  foreignKey: 'expenseCategoryid',
-});
+// ExpenseCategoryModel.hasMany(ExpenseModel, {
+//   foreignKey: 'expenseCategoryid',
+// })
+// ExpenseModel.belongsTo(ExpenseCategoryModel, {
+//   foreignKey: 'expenseCategoryid',
+// });
 
-ExpenseModel.belongsTo(EmployeeModel, {
-  foreignKey: 'employeeId',
-});
-EmployeeModel.hasMany(ExpenseModel, {
-  foreignKey: 'employeeId',
-});
+// ExpenseModel.belongsTo(EmployeeModel, {
+//   foreignKey: 'employeeId',
+// });
+// EmployeeModel.hasMany(ExpenseModel, {
+//   foreignKey: 'employeeId',
+// });
 // empolyee and project
 EmployeeModel.belongsTo(ProjectModel, {
   foreignKey: 'projectId',
@@ -166,38 +166,38 @@ ProjectModel.hasMany(EmployeeModel, {
   foreignKey: 'projectId',
 });
 
-//customer project and order
-CustomerModel.belongsTo(ProjectModel, {
-  foreignKey: 'projectId',
-});
+// //customer project and order
+// CustomerModel.belongsTo(ProjectModel, {
+//   foreignKey: 'projectId',
+// });
 
-ProjectModel.hasMany(CustomerModel, {
-  foreignKey: 'projectId',
-});
+// ProjectModel.hasMany(CustomerModel, {
+//   foreignKey: 'projectId',
+// });
 
-CustomerModel.hasMany(OrderModel, {
-  foreignKey: 'customerId',
-});
+// CustomerModel.hasMany(OrderModel, {
+//   foreignKey: 'customerId',
+// });
 
-OrderModel.belongsTo(CustomerModel, {
-  foreignKey: 'customerId',
-});
-
-
+// OrderModel.belongsTo(CustomerModel, {
+//   foreignKey: 'customerId',
+// });
 
 
 
 
 
-// const createTable = async () => {
-//   try {
-//     await sequelize.sync({ force: true });
-//     console.log("table cereated seuccessfully.");
-//   } catch (error) {
-//     console.error("Error creating tables:", error);
-//   } finally {
-//     await sequelize.close();
-//   }
-// };
 
-// module.exports = createTable;
+
+const createTable = async () => {
+  try {
+    await sequelize.sync({ force: true });
+    console.log("table cereated seuccessfully.");
+  } catch (error) {
+    console.error("Error creating tables:", error);
+  } finally {
+    await sequelize.close();
+  }
+};
+
+module.exports = createTable;
